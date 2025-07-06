b7de657aaf61f947ec6c526f559f01c65c80b217
Jesús Castillo Vidal <jcdev2004@gmail.com>
2025-07-06 10:16:10 -0500

    refactor(env): now using env instead of json file

--- rutasperu_backend/firebase_config.py
+++ rutasperu_backend/firebase_config.py
@@ -1,10 +1,27 @@
 import os
+import json
+import tempfile
 import firebase_admin
 from firebase_admin import credentials, firestore
-from django.conf import settings
 
-# Ruta al JSON; si usas GOOGLE_APPLICATION_CREDENTIALS, firebase-admin lo detecta solo
-cred_path = os.path.join(settings.BASE_DIR, 'service_key.json')
-cred = credentials.Certificate(cred_path)
+# Obtener el JSON desde la variable de entorno
+json_str = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+
+if not json_str:
+    raise ValueError("La variable de entorno 'GOOGLE_APPLICATION_CREDENTIALS_JSON' no está definida.")
+
+# Convertir a diccionario
+json_data = json.loads(json_str)
+
+# Reemplazar los `\\n` por saltos de línea reales en la private_key
+json_data["private_key"] = json_data["private_key"].replace("\\n", "\n")
+
+# Guardar como archivo temporal válido
+with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".json") as temp_file:
+    json.dump(json_data, temp_file)
+    temp_file_path = temp_file.name
+
+# Inicializar Firebase con el archivo temporal
+cred = credentials.Certificate(temp_file_path)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
